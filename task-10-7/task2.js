741e7af5c8cb14c28c68c11bcfbb05c0d03790e8
laithy0 <laithyaseen955@gmail.com>
2023-07-11 06:00:52 +0300

    task10-7

---
+++ task-10-7/task2.js
@@ -0,0 +1,15 @@
+var row = 4;
+var num = 1;
+
+for(var a = 0 ; a < 4 ; a++){
+
+    var x = " ";
+    
+    for(var i = 0 ; i <= a ; i++){
+
+        x = x + num + " ";
+        num++;
+
+    }
+    console.log(x)
+}
